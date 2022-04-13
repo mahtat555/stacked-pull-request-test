01d28e0329bf1f6cd6c0291c43609a06d5c30e0a
mahtat555 <yassinmhtat@gmail.com>
2022-04-13 22:14:17 +0000

    Setup the backend

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log("Hello from index !!")
+const http = require("http");
+
+// Server configuration
+const host = 'localhost';
+const port = 8000;
+
+
+const requestListener = (req, res) => {
+    res.setHeader("Content-Type", "application/json");
+    res.writeHead(200);
+    res.end(JSON.stringify({
+        message: "This is a JSON response"
+    }));
+};
+
+// Create the server
+const server = http.createServer(requestListener);
+
+// Start the Server
+server.listen(port, host, () => {
+    console.log(`Server is running on http://${host}:${port}`);
+});
