d0859b8dc34e938ec7313c6364d66f9b7f60040c
mahtat555 <yassinmhtat@gmail.com>
2022-04-13 23:00:53 +0000

    Add inverse modulo calculator

---
+++ mathLogic/inverse.js
@@ -0,0 +1,25 @@
+/**
+ * Inverse Modulo Calculator
+ * @param {integer} integer
+ * @param {integer} modulo
+ * @returns {integer}
+ */
+const inverseModulo = (integer, modulo) => {
+    let a = integer, b = modulo;
+    let u = 1, v = 0;
+
+    do {
+        r = a % b;
+        q = Math.floor(a / b);
+        a = b;
+        b = r;
+        [v, u] = [u - v * q, v];
+    } while (r != 0);
+
+    return (modulo + u) % modulo;
+}
+
+
+module.exports = {
+    inverseModulo
+}
